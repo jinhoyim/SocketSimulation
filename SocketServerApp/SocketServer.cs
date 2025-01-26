03f8d97f675be8b61d3a74f431cb3f2303de51c6
Jinho Yim <jinhoyim@outlook.com>
2025-01-26 23:03:11 +0900

    메서드 추출

--- SocketServerApp/SocketServer.cs
+++ SocketServerApp/SocketServer.cs
@@ -12,6 +12,7 @@ namespace SocketServerApp
         private readonly CancellationTokenSource _cts;
 
         private bool _isStarted = false;
+        private readonly int startMinConnections = 5;
         private readonly Lock _lock = new Lock();
         private readonly ConcurrentDictionary<string, Socket> _clients = new();
 
@@ -30,16 +31,21 @@ namespace SocketServerApp
         {
             var cancellationToken = _cts.Token;
             
-            using var listener = new Socket(
-                        _ipEndPoint.AddressFamily,
-                        SocketType.Stream,
-                        ProtocolType.Tcp);
+            var listener = new Socket(
+                _ipEndPoint.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
             listener.LingerState = new LingerOption(true, LingerTimeSeconds);
-            listener.Bind(_ipEndPoint);
-            listener.Listen(1000);
+            await ListenAsync(listener, cancellationToken);
+        }
 
+        private async Task ListenAsync(Socket listener, CancellationToken cancellationToken)
+        {
             try
             {
+                listener.Bind(_ipEndPoint);
+                listener.Listen(1000);
+                
                 while (!cancellationToken.IsCancellationRequested)
                 {
                     var client = await listener.AcceptAsync(cancellationToken);
@@ -47,41 +53,7 @@ namespace SocketServerApp
 
                     _ = Task.Run(async () =>
                     {
-                        string clientId = string.Empty;
-                        var connectionAcceptor = new ConnectionAcceptor(client, _clients);
-                        bool accepted = false;
-                        
-                        try
-                        {
-                            (accepted, clientId) = await connectionAcceptor.AcceptAsync(cancellationToken);    
-                            if (!accepted)
-                            {
-                                return;
-                            }
-                            _clients[clientId] = client;
-                        }
-                        catch (SocketException se)
-                        {
-                            Console.WriteLine($"Socket Error : {se.SocketErrorCode}({se.ErrorCode}) {se.Message}");
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine($"System Error : {ex}");
-                        }
-                        finally
-                        {
-                            if (client.Connected)
-                            {
-                                client.Shutdown(SocketShutdown.Both);
-                            }
-                            client.Dispose();
-
-                            _clients.TryRemove(clientId, out _);
-                            if (_clients.IsEmpty)
-                            {
-                                await _cts.CancelAsync();
-                            }
-                        }
+                        await ClientHandleAsync(client, cancellationToken);
                         Console.WriteLine("Client Socket closed.");
                     }, cancellationToken);
                 }
@@ -102,8 +74,61 @@ namespace SocketServerApp
                 {
                     listener.Shutdown(SocketShutdown.Both);
                 }
+                listener.Dispose();
             }
             Console.WriteLine("Listener socket closed.");
+        }
+
+        private async Task ClientHandleAsync(Socket client, CancellationToken cancellationToken)
+        {
+            string clientId = string.Empty;
+            try
+            {
+                var connectionAcceptor = new ConnectionAcceptor(client, _clients);
+                (var accepted, clientId) = await connectionAcceptor.AcceptAsync(cancellationToken);
+                if (accepted)
+                {
+                    await ReceiveAsync(client, clientId, cancellationToken);
+                }
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine($"Socket Error : {se.SocketErrorCode}({se.ErrorCode}) {se.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"System Error : {ex}");
+            }
+            finally
+            {
+                if (client.Connected)
+                {
+                    client.Shutdown(SocketShutdown.Both);
+                }
+                client.Dispose();
+
+                _clients.TryRemove(clientId, out _);
+                if (_clients.IsEmpty)
+                {
+                    await _cts.CancelAsync();
+                }
+            }
+        }
+
+        private async Task ReceiveAsync(Socket client, string clientId, CancellationToken cancellationToken)
+        {
+            _clients[clientId] = client;
+                        
+            bool isStarted;
+            lock (_lock)
+            {
+                isStarted = _isStarted;
+            }
+
+            if (!isStarted && _clients.Count >= startMinConnections)
+            {
+                
+            }
         }
     }
 }
